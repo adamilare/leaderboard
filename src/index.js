36e9aa90c04916c55912c264c15cb1c2b8e6320f
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-03-08 19:52:38 +0100

    Add leaderboard API calls and render

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import './style.css';
+import { getScores, submitScore } from './games.js';
 
-function component() {
-  const element = document.createElement('div');
+const scoresListElement = document.querySelector('#recent-scores');
+const submitBtn = document.querySelector('button[type="submit"]');
+const refreshBtn = document.querySelector('#refresh');
+const scoreItem = (name, score) => `<li class="score-item" data-ref="">
+            <span class="name">${name}:</span><span class="score">${score}</span>
+          </li>`;
 
-  element.classList.add('hello');
+const renderScores = async () => {
+  const scores = await getScores();
+  if (scores) {
+    scoresListElement.innerHTML = scores.result
+      .map((score) => scoreItem(score.user, score.score))
+      .join('');
+  }
+};
 
-  return element;
-}
+const submit = async () => {
+  const name = document.querySelector('input[name="name"]');
+  const score = document.querySelector('input[name="score"]');
 
-document.body.appendChild(component());
+  if (name && score) await submitScore(name.value, score.value);
+  name.value = '';
+  score.value = '';
+  renderScores();
+};
+
+refreshBtn.addEventListener('click', () => renderScores());
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  submit();
+});
+
+renderScores();
