e32dc14d75b2248cd906624d4cde355117ace30f
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-03-08 07:59:02 +0100

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import _ from "lodash";
-import "./style.css";
+import './style.css';
 
 function component() {
-  const element = document.createElement("div");
+  const element = document.createElement('div');
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
+  element.classList.add('hello');
 
   return element;
 }
